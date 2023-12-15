091a2a3d4e1c8294638778467318c4725db4834b
Stella Erdmann <erdmann@imbi.uni-heidelberg.de>
2023-12-15 10:15:04 +0100

    update DC_plot_group

--- application/R/DC_plot_group.R
+++ application/R/DC_plot_group.R
@@ -7,9 +7,12 @@
 #' @return ggsurvplot object
 #'
 #' @examples
-#' data <- data.frame(start_date = as.Date(rep(0, 100), origin = "2022-01-01"),
-#' final_date = as.Date(round(runif(100, 0, 250)), origin = "2022-01-01"),
-#' event = rbinom(100, 1, 0.9))
+#' data <- data.frame(start_date = c(as.Date(rep(0, 200), origin = "2022-01-01"),
+#' as.Date(rep(0, 200), origin = "2022-01-01")),
+#' final_date = c(as.Date(round(runif(200, 0, 250)), origin = "2022-01-01"),
+#'                as.Date(round(runif(200, 0, 450)), origin = "2022-01-01")),
+#' event = c(rbinom(200, 1, 0.9), rbinom(200, 1, 0.9)),
+#' group = c(rep(0, 200), rep(1, 200)))
 #' DC_plot_group(data)
 #'
 #' @export
@@ -30,16 +33,9 @@ DC_plot_group <- function(data){
   DF_norm0 <- subset(DF_norm, group == 0)
   DF_norm1 <- subset(DF_norm, group == 1)
 
-
   # maximum event time
-  # maximum event time
-  data0 <- subset(data, group == 0)
-  data1 <- subset(data, group == 1)
-
-  maximum_event_time0 <- ceiling(max(difftime(data0$final_date,
-                                              data0$start_date)))
-  maximum_event_time1 <- ceiling(max(difftime(data1$final_date,
-                                              data1$start_date)))
+  maximum_event_time0 <- ceiling(max(DF_norm0$time))
+  maximum_event_time1 <- ceiling(max(DF_norm1$time))
 
   DF_norm0 <- subset(DF_norm0, time <= maximum_event_time0)
   keep0    <- as.numeric(names(which(table(DF_norm0$time) == 3)))
@@ -70,18 +66,18 @@ DC_plot_group <- function(data){
   # partial difference curves to indicate directional instability:
   # the difference between the upper limit and the Kaplan–Meier estimate and
   diff_upper_KM0 <- subset(DF_norm0, strata == "measure=SI upper bound, group=0")$surv -
-    subset(DF_norm0, strata == "KM estimate")$surv
+    subset(DF_norm0, strata == "measure=KM estimate, group=0")$surv
 
   # the difference between the Kaplan–Meier estimate and the lower limit.
-  diff_KM_lower0 <- subset(DF_norm0, strata == "KM estimate")$surv -
+  diff_KM_lower0 <- subset(DF_norm0, strata == "measure=KM estimate, group=0")$surv -
     subset(DF_norm0, strata == "measure=SI lower bound, group=0")$surv
 
-  diff_upper_KM1 <- subset(DF_norm1, strata == "measure=SI upper bound, group=0")$surv -
-    subset(DF_norm1, strata == "KM estimate")$surv
+  diff_upper_KM1 <- subset(DF_norm1, strata == "measure=SI upper bound, group=1")$surv -
+    subset(DF_norm1, strata == "measure=KM estimate, group=1")$surv
 
   # the difference between the Kaplan–Meier estimate and the lower limit.
-  diff_KM_lower1 <- subset(DF_norm1, strata == "KM estimate")$surv -
-    subset(DF_norm1, strata == "measure=SI lower bound, group=0")$surv
+  diff_KM_lower1 <- subset(DF_norm1, strata == "measure=KM estimate, group=1")$surv -
+    subset(DF_norm1, strata == "measure=SI lower bound, group=1")$surv
 
   # Figure 4. Difference curve between upper and lower limits of Kaplan–Meier
   # and partial difference curves between Kaplan–Meier and upper and lower limits
@@ -112,6 +108,8 @@ DC_plot_group <- function(data){
                                               "1: SI upper limit minus SI lower limit",
                                               "1: SI upper limit minus KM estmate",
                                               "1: KM estimate minus SI lower limit"))
+  DF4$group <- as.factor(DF4$group)
+
 
   plot4 <- ggplot2::ggplot(data = DF4,
                   ggplot2::aes(x = time,
@@ -120,19 +118,19 @@ DC_plot_group <- function(data){
     # ggplot2::ggtitle("Difference curve between upper and lower limits of Kaplan–Meier and partial difference curves between Kaplan–Meier and upper and lower limits") +
     ggplot2::geom_line(ggplot2::aes(linetype = strata, color = group)) +
     ggplot2::scale_linetype_manual(name="", values = c("solid",
-                                              "dashed",
-                                              "dotted",
-                                              "solid",
-                                              "dashed",
-                                              "dotted"),
-                          labels=c(paste0("0: SI upper limit - SI lower limit;\n normalized auc0 = ",
-                                          round(norm_auc0, 2 )),
-                                   "0: SI upper limit - KM estmate",
-                                   "0: KM estimate - SI lower limit",
-                          paste0("1: SI upper limit - SI lower limit;\n normalized auc1 = ",
-                                 round(norm_auc1, 2 )),
-                          "1: SI upper limit - KM estmate",
-                          "1: KM estimate - SI lower limit")) +
+                                                       "dashed",
+                                                       "dotted",
+                                                       "solid",
+                                                       "dashed",
+                                                       "dotted"),
+                                   labels=c(paste0("0: SI upper limit - SI lower limit;\n normalized auc0 = ",
+                                                   round(norm_auc0, 2 )),
+                                            "0: SI upper limit - KM estmate",
+                                            "0: KM estimate - SI lower limit",
+                                            paste0("1: SI upper limit - SI lower limit;\n normalized auc1 = ",
+                                                   round(norm_auc1, 2 )),
+                                            "1: SI upper limit - KM estmate",
+                                            "1: KM estimate - SI lower limit")) +
     survminer::theme_survminer()
 
   return(plot4)
